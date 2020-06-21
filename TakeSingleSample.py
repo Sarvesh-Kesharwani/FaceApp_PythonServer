9778afa2c6999d60435ecc77c63958171ad2dbff
sarvesh kesharwani <sarveshkumar5513@gmail.com>
2020-06-21 07:21:04 -0400

    TakeSample.py updated

--- TakeSingleSample.py
+++ TakeSingleSample.py
@@ -1,22 +1,22 @@
-import face_recognition_models
+import face_recognition
 import os
 import pickle
 
 def TakeSamples(dir,person):
 
-    all_face_encodings = {}
+    face_encodings = {}
     face = face_recognition.load_image_file(dir)
     #calculate no. of face in sample-image
     face_bounding_boxes = face_recognition.face_locations(face)
     no_of_faces = len(face_bounding_boxes)
 
     if no_of_faces == 1:
-        all_face_encodings[person] = face_recognition.face_encodings(face)[0]
+        face_encodings[person] = face_recognition.face_encodings(face)[0]
     else:
         print(person + "_img contains multiple faces!")
 
-    with open('dataset_faces.dat', 'a+') as f:
-        pickle.dump(all_face_encodings, f)
+    with open('dataset_faces.dat', 'wb') as f:
+        pickle.dump(face_encodings, f)
 
 
 
